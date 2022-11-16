3912cded5a4b8d1fb7305bb28fc9308d3581ad96
Sachin dubey <only2sachd@gmail.com>
2022-11-16 19:30:54 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,20 @@
+const socket= io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInp = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container');
+
+const append = (message,position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+const name= prompt("Enter your name to join");
+socket.emit('new-user-joined',name);
+
+socket.on('user-joined' ,name=>{
+    append('${name} joined the chat', 'right');
+})
